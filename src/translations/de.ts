67259071a66102178a341ebc753959a764aad312
Daniel Mulholland <dan.mulholland@gmail.com>
2023-02-26 08:41:16 +1300

    Allow filtering of only configured ExtRefs in subscriber view

--- src/translations/de.ts
+++ src/translations/de.ts
@@ -348,6 +348,7 @@ export const de: Translations = {
     disconnect: 'Daten-Attribute Verbindung gelöst',
     subscriber: {
       subscribed: 'Verbunden',
+      onlyConfigured: 'Nur Konfiguriert',
       availableToSubscribe: 'Kann verbunden werden',
       partiallySubscribed: 'Teilweise verbunden',
       noControlBlockSelected: 'Keine Kontrollblock ausgewählt',
