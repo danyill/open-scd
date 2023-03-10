761f882b3610bda39e7f316fecd57f9fe1c6709f
Daniel Mulholland <dan.mulholland@gmail.com>
2023-03-11 08:27:00 +1300

    Add Transpower theme

--- src/themes.ts
+++ src/themes.ts
@@ -7,8 +7,8 @@ export function getTheme(theme: Settings['theme']): TemplateResult {
     ${themes[theme]}
     <style>
       * {
-        --primary: var(--cyan);
-        --secondary: var(--violet);
+        --primary: #0295db;
+        --secondary: #85c700;
         --mdc-theme-primary: var(--primary);
         --mdc-theme-secondary: var(--secondary);
         --mdc-theme-background: var(--base3);
@@ -63,28 +63,28 @@ export function getTheme(theme: Settings['theme']): TemplateResult {
 
 const bodyStyles: Record<Settings['theme'], string> = {
   dark: 'background: #073642',
-  light: 'background: #eee8d5',
+  light: 'background: #f1fbff',
 };
 
 const themes: Record<Settings['theme'], TemplateResult> = {
   light: html`
     <style>
       * {
-        --base03: #002b36;
-        --base02: #073642;
-        --base01: #586e75;
-        --base00: #657b83;
-        --base0: #839496;
-        --base1: #93a1a1;
-        --base2: #eee8d5;
-        --base3: #fdf6e3;
-        --yellow: #b58900;
-        --orange: #cb4b16;
-        --red: #dc322f;
-        --magenta: #d33682;
-        --violet: #6c71c4;
-        --blue: #268bd2;
-        --cyan: #2aa198;
+        --base0: #0b3569;
+        --base1: #193e6a;
+        --base2: #e6f5fd;
+        --base3: #f6fbff;
+        --base00: #4d476b;
+        --base01: #30295a;
+        --base02: #474263;
+        --base03: #443f5c;
+        --yellow: #ffc107;
+        --orange: #fd7e14;
+        --red: #dc3545;
+        --magenta: #d63384;
+        --violet: #6f42c1;
+        --blue: #0dcaf0;
+        --cyan: #20c997;
         --green: #859900;
       }
     </style>
